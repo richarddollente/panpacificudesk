0ea73f426c7a0f5d40541837a9ee8fc58b0a04d0
richarddollente <dollente.richard@panpacificu.edu.ph>
2022-02-08 01:06:54 -0800

    Adjusting UserFactory for TestUnit Purposes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,13 +2,24 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
     protected $model = User::class;
 
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
     public function definition()
     {
         return [
@@ -19,5 +30,4 @@ class UserFactory extends Factory
             'remember_token' => Str::random(10),
         ];
     }
-
 }
