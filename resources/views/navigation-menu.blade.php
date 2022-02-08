856ff51d3ea5c9d94612caaf0a0ad398e90a5f16
richarddollente <dollente.richard@panpacificu.edu.ph>
2022-02-08 00:57:07 -0800

    Adding and adjusting Main views

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,9 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
-                    </a>
+                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                 </div>
 
                 <!-- Navigation Links -->
