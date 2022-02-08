c309867303c17a7025f8f2576bb55d696574eaa5
richarddollente <dollente.richard@panpacificu.edu.ph>
2022-02-08 01:04:10 -0800

    Adjusted Layout\App and Guest views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,20 +6,21 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Panpacific University Helpdesk</title>
-        <link rel="icon" href="school-logo.jpg" type="image/icon type">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        @livewireStyles
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            @livewire('navigation-dropdown')
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
@@ -33,5 +34,9 @@
                 {{ $slot }}
             </main>
         </div>
+
+        @stack('modals')
+
+        @livewireScripts
     </body>
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,16 +6,15 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Panpacific University Helpdesk</title>
-        <link rel="icon" href="school-logo.jpg" type="image/icon type">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
