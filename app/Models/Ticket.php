15b87a82399056477fa6a2fbe2575e61e5874762
richarddollente <dollente.richard@panpacificu.edu.ph>
2022-02-13 14:38:23 -0800

    Added implementation of user_id token to Ticket Model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,22 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Ticket extends Model
+class Ticket extends Model implements HasMedia
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, InteractsWithMedia;
 
     protected $fillable =[
         'subject',
-        'status',
-        'priority',
         'description',
-        'assigned_tech'
+        'user_id',
+        'admin_id',
+    ];
+
+    protected $guarded = [
+
     ];
 
     public function users()
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')->width(50)->height(50);
+    }
+
+    public function getAttachmentsAttribute()
+    {
+        return $this->getMedia('file');
     }
 
 }
