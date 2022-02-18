18d3b2cfa44a8e60f67255db0f371d5cf4c62d6b
richarddollente <dollente.richard@panpacificu.edu.ph>
2022-02-17 22:12:39 -0800

    modified Ticket Controller to pass image on database and views

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Ticket;
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Status;
+use App\Models\Priority;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,7 +20,8 @@ class TicketController extends Controller
 {
     public function index()
     {
-        $tickets = Ticket::all();
+
+        $tickets = Ticket::with('users')->get();
         if(Gate::allows('ticket_access')){
             $tickets = Ticket::where('user_id', Auth::user()->id)->get();
             return view('tickets.index', compact('tickets'));
@@ -25,6 +31,7 @@ class TicketController extends Controller
             return view('tickets.staff', compact('tickets'));
         }
         return view('tickets.admin', compact('tickets'));
+
     }
 
     public function create()
@@ -34,15 +41,29 @@ class TicketController extends Controller
 
     public function store(Request $request)
     {
-        $ticket = Ticket::create([
-            'user_id' => Auth::user()->id,
-            'subject' => request('subject'),
-            'description' => request('description'),
-            'admin_id' => 1,
+        $hasFile = $request->hasFile('ticket_file');
+        if($hasFile){
+            $file = $request->file('ticket_file');
+            dump($filePath = Storage::disk('public')->putFile('ticket_files', $file));
+            dump(Ticket::create([
+                'user_id' => Auth::user()->id,
+                'subject' => request('subject'),
+                'description' => request('description'),
+                'admin_id' => 1,
+                'category_id' => 1,
+                'ticket_file' => $filePath,
+            ]));
 
-        ]);
-        if($request->hasFile('file') && $request->file('file')->isValid()){
-            $ticket->addMediaFormRequest('file')->toMediaCollection('file');
+        }
+        else
+        {
+            Ticket::create([
+                'user_id' => Auth::user()->id,
+                'subject' => request('subject'),
+                'description' => request('description'),
+                'admin_id' => 1,
+                'category_id' => 1,
+            ]);
         }
 
         return redirect()->route(route:'tickets.index');
@@ -50,7 +71,12 @@ class TicketController extends Controller
 
     public function show(Ticket $ticket)
     {
-        return view('tickets.index', compact('ticket'));
+        $username = DB::table('users')->where('id', ($ticket->user_id))->value('name');
+        $category = DB::table('categories')->where('id', ($ticket->category_id))->value('title');
+        $status = DB::table('statuses')->where('id', ($ticket->status_id))->value('title');
+        $priority = DB::table('priorities')->where('id', ($ticket->priority_id))->value('title');
+        $admin = DB::table('users')->where('id', ($ticket->admin_id))->value('name');
+        return view('tickets.show', compact('ticket', 'username', 'category', 'status', 'priority', 'admin'));
     }
 
     public function edit(Ticket $ticket)
