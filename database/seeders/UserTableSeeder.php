9ea34600e4d3553a16972b2aa62e804882709cfa
richarddollente <dollente.richard@panpacificu.edu.ph>
2022-02-20 11:37:45 -0800

    Updated UserTable Seeder with class_id

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -14,25 +14,29 @@ class UserTableSeeder extends Seeder
                 'id'    => 1,
                 'name'  => "Richard Dollente",
                 'email' => 'admin.dollente@panpacificudesk.com',
-                'password' => bcrypt('password')
+                'password' => bcrypt('password'),
+                'class_id' => 1,
             ],
             [
                 'id'    => 2,
                 'name'  => "Jester Apelado",
                 'email' => 'user.apelado@panpacificudesk.com',
-                'password' => bcrypt('password')
+                'password' => bcrypt('password'),
+                'class_id' => 3,
             ],
             [
                 'id'    => 3,
                 'name'  => "Rosemarie Ancheta",
                 'email' => 'staff.ancheta@panpacificudesk.com',
-                'password' => bcrypt('password')
+                'password' => bcrypt('password'),
+                'class_id' => 1,
             ],
             [
                 'id'    => 4,
                 'name'  => "Jhovbert Bogtong",
                 'email' => 'admin.bogtong@panpacificudesk.com',
-                'password' => bcrypt('password')
+                'password' => bcrypt('password'),
+                'class_id' => 1,
             ]
         ];
 
