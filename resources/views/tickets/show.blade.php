67357bad8a7591180ad7d6746411f8b3440ac03d
richarddollente <dollente.richard@panpacificu.edu.ph>
2022-02-20 11:39:55 -0800

    Adjusted Ticket/Show View with Comments

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -109,14 +109,16 @@
                     </div>
                 </div>
             </div>
-            <div class="flex flex-col">
+            <div class="flex flex-col" id="comment-section">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                             <table class="min-w-full divide-y divide-gray-200 w-full">
                                 <tr class="border-b">
                                     <th scope="col" class="px-6 py-3 bg-white text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Comment Section
+                                        <a name="CommentSection">
+                                            Comment Section
+                                        </a>
                                     </th>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 bg-white divide-y divide-gray-200">
 
@@ -138,17 +140,16 @@
                                 </tr>
                                 @endforeach
                             </table>
-                            <form method="post" action="/tickets/{id}/comments">
+                            <form method="post" action="/tickets/{{ $ticket->id }}/comments">
                                 @csrf
-                                    <div class="px-4 py-5 bg-white sm:p-6">
-                                        <label for="body" class="block font-medium text-sm text-gray-700">Post Your Comment:</label>
-                                        <textarea type="text" name="body" id="body" class="form-input rounded-md shadow-sm mt-1 block w-full"/></textarea>
-                                    </div>
-                                    <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                        <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
-                                            Post Comment
-                                        </button>
-                                    </div>
+                                <div class="px-4 py-5 bg-white sm:p-6">
+                                    <a name="CommentBody" for="body" class="block font-medium text-sm text-gray-700">Post Your Comment:</a>
+                                    <textarea type="text" name="body" id="body" class="form-input rounded-md shadow-sm mt-1 block w-full" required></textarea>
+                                </div>
+                                <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                    <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                                        Post Comment
+                                    </button>
                                 </div>
                             </form>
                         </div>
