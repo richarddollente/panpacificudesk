2cf43cbcc3f06c5c790d6c2fe1013547ee3c4bad
richarddollente <dollente.richard@panpacificu.edu.ph>
2022-03-29 00:57:54 -0700

    Added CommentTicketNotification

---
+++ app/Notifications/CommentTicketNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Ticket;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class CommentTicketNotification extends Notification
+{
+    use Queueable;
+
+    public $user;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+
+        ];
+    }
+
+    public function toDatabase($notifiable)
+    {
+        return [
+            'username' => (DB::table('users')->where('id', (Auth::user()->id))->value('name')),
+            'ticket_id' => $this->comment->ticket_id,
+            'comment_id' => $this->comment->id,
+        ];
+    }
+}
